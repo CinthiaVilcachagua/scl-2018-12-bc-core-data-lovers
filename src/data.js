276dc2747280da3bb06118cc76f11ff915b9725d
CinthiaVilcachagua <cv.ccori@gmail.com>
2019-01-31 14:33:59 -0500

    añadiendo la función de sort con su respectivo test.

--- src/data.js
+++ src/data.js
@@ -2,6 +2,24 @@ const filterData = (data, condition) => {
   return data.filter(element => (element.indicatorCode.indexOf(condition) !== -1));
 };
 
+const sortData = (data, sortBy, sortOrder) => {
+  const indicatorsSorts = data.sort((a,b) => { 
+    if (a[sortBy] > b[sortBy]) {
+       return 1;
+    }if (a[sortBy] < b[sortBy]) {
+       return -1;
+    } else {
+       return 0;
+    }
+   }); 
+ if (sortOrder === 'ascendente') {
+    return indicatorsSorts;
+ } else {
+    return indicatorsSorts.reverse();
+ }
+};
+
 window.worldbank = {
   filterData,
+  sortData,
 };
